1e4804679fd6a5f98091e72b061ac41f3fe50bfc
Dan Jacobellis <danjacobellis@utexas.edu>
2021-11-14 00:40:49 -0600

    rebuild

--- pages/seq_time.py
+++ pages/seq_time.py
@@ -1,13 +1,26 @@
 import librosa
+import numpy as np
 import time
-y,sr = librosa.load("tetris_piano_mono.wav")
-t1 = time.time()
-S = librosa.feature.melspectrogram(
-    y=y,
-    sr=sr,
-    n_fft=16384,
-    hop_length=128,
-    n_mels=8192,
-)
-t2 = time.time()
-print(t2-t1)
+y,sr = librosa.load("three_minute_warning.wav")
+for N in [2**15,2**18,2**22,2**26]:
+    t1 = time.time()
+    x_i = y[0:N]
+    S = librosa.feature.melspectrogram(
+        y=x_i,
+        sr=sr,
+        n_fft=2^20,
+        hop_length=32,
+        n_mels=512,
+    )
+    t2 = time.time()
+    print(t2-t1,"\n")
+    
+    t1 = time.time()
+    y_rec = librosa.feature.inverse.mel_to_audio(
+        M=S,
+        sr=sr,
+        n_fft=2^20,
+        hop_length=32,
+    )
+    t2 = time.time()
+    print(t2-t1,"\n")
